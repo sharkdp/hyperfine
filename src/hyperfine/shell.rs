308212df1218000c0280c1edb709e6688a775d6e
Steve Pentland <stevepentland@gmail.com>
2018-02-07 19:03:52 -0500

    Refactor execute and measure to shell, abstract away cpu timer

--- src/hyperfine/shell.rs
+++ src/hyperfine/shell.rs
@@ -1,11 +1,56 @@
 use std;
 use std::io;
-use std::process::{Command, Stdio};
+use std::process::{Command, ExitStatus, Stdio};
 
+use hyperfine::timer::get_cpu_timer;
+
+/// Used to indicate the result of running a command
+#[derive(Debug, Copy, Clone)]
+pub struct ExecuteResult {
+    /// The amount of user time the process used
+    pub user_time: f64,
+
+    /// The amount of cpu time the process used
+    pub system_time: f64,
+
+    /// The exit status of the process
+    pub status: ExitStatus,
+}
+
+/// Execute the given command and return a timing summary
+#[cfg(windows)]
+pub fn execute_and_time(command: &str) -> io::Result<ExecuteResult> {
+    let mut child = run_shell_command(command)?;
+    let cpu_timer = get_cpu_timer(&child);
+    let status = child.wait()?;
+
+    let (user_time, system_time) = cpu_timer.stop();
+    Ok(ExecuteResult {
+        user_time,
+        system_time,
+        status,
+    })
+}
+
+/// Execute the given command and return a timing summary
+#[cfg(not(windows))]
+pub fn execute_and_time(command: &str) -> io::Result<ExecuteResult> {
+    let cpu_timer = get_cpu_timer();
+
+    let status = run_shell_command(command)?;
+
+    let (user_time, system_time) = cpu_timer.stop();
+
+    Ok(ExecuteResult {
+        user_time,
+        system_time,
+        status,
+    })
+}
 
 /// Run a standard shell command
 #[cfg(not(windows))]
-pub fn run_shell_command(command: &str) -> io::Result<std::process::ExitStatus> {
+fn run_shell_command(command: &str) -> io::Result<std::process::ExitStatus> {
     Command::new("sh")
         .arg("-c")
         .arg(command)
@@ -17,7 +62,7 @@ pub fn run_shell_command(command: &str) -> io::Result<std::process::ExitStatus> 
 
 /// Run a Windows shell command using cmd.exe
 #[cfg(windows)]
-pub fn run_shell_command(command: &str) -> io::Result<std::process::Child> {
+fn run_shell_command(command: &str) -> io::Result<std::process::Child> {
     Command::new("cmd")
         .arg("/C")
         .arg(command)
