717fa4658682dc8193819c542018dc5f1530e292
Steve Pentland <stevepentland@gmail.com>
2018-03-17 20:21:45 -0400

    Do not copy export items between exporters, write from central vec

--- src/hyperfine/export/csv.rs
+++ src/hyperfine/export/csv.rs
@@ -4,18 +4,13 @@ use csv::WriterBuilder;
 use std::io::Result;
 
 pub struct CsvExporter {
-    results: Vec<ExportEntry>,
     out_file: String,
 }
 
 impl ResultExporter for CsvExporter {
-    fn add_entry(&mut self, entry: ExportEntry) {
-        self.results.push(entry);
-    }
-
-    fn write(&self) -> Result<()> {
+    fn write(&self, results: &Vec<ExportEntry>) -> Result<()> {
         let mut writer = WriterBuilder::new().from_path(&self.out_file)?;
-        for res in &self.results {
+        for res in results {
             writer.serialize(res)?;
         }
         Ok(())
@@ -25,7 +20,6 @@ impl ResultExporter for CsvExporter {
 impl CsvExporter {
     pub fn new(file_name: String) -> Self {
         CsvExporter {
-            results: Vec::new(),
             out_file: file_name,
         }
     }
