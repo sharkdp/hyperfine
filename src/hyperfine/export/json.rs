1bd543610936ca850a999020770a7bb0778e24ab
sharkdp <davidpeter@web.de>
2018-03-20 21:12:33 +0100

    Proper error handling for the exporter

--- src/hyperfine/export/json.rs
+++ src/hyperfine/export/json.rs
@@ -13,14 +13,7 @@ pub struct JsonExporter {}
 
 impl Exporter for JsonExporter {
     fn serialize(&self, results: &Vec<ExportEntry>) -> Result<String> {
-        let serialized = to_string_pretty(&HyperfineSummary { results });
-
-        serialized.map_err(|e| {
-            Error::new(
-                ErrorKind::Other,
-                format!("Error while serializing to JSON: {:}", e),
-            )
-        })
+        to_string_pretty(&HyperfineSummary { results }).map_err(|e| Error::new(ErrorKind::Other, e))
     }
 }
 
