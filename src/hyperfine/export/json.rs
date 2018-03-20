8b933dc64da091a160e6bd49fc0144008f50057e
Steve Pentland <stevepentland@gmail.com>
2018-03-20 17:00:24 -0400

    Add markdown support.
    Don't create Strings to convert to bytes later, just serialize directly
    to bytes in the first place

--- src/hyperfine/export/json.rs
+++ src/hyperfine/export/json.rs
@@ -2,23 +2,18 @@ use super::{ExportEntry, Exporter};
 
 use std::io::{Error, ErrorKind, Result};
 
-use serde_json::to_string_pretty;
+use serde_json::to_vec_pretty;
 
 #[derive(Serialize, Debug)]
 struct HyperfineSummary<'a> {
     results: &'a Vec<ExportEntry>,
 }
 
+#[derive(Default)]
 pub struct JsonExporter {}
 
 impl Exporter for JsonExporter {
-    fn serialize(&self, results: &Vec<ExportEntry>) -> Result<String> {
-        to_string_pretty(&HyperfineSummary { results }).map_err(|e| Error::new(ErrorKind::Other, e))
-    }
-}
-
-impl JsonExporter {
-    pub fn new() -> Self {
-        JsonExporter {}
+    fn serialize(&self, results: &Vec<ExportEntry>) -> Result<Vec<u8>> {
+        to_vec_pretty(&HyperfineSummary { results }).map_err(|e| Error::new(ErrorKind::Other, e))
     }
 }
---
+++ src/hyperfine/export/markdown.rs
@@ -0,0 +1,38 @@
+use super::{ExportEntry, Exporter};
+
+use std::io::Result;
+
+const MULTIPLIER: f64 = 1e3;
+
+#[derive(Default)]
+pub struct MarkdownExporter {}
+
+impl Exporter for MarkdownExporter {
+    fn serialize(&self, results: &Vec<ExportEntry>) -> Result<Vec<u8>> {
+        let mut destination = start_table();
+
+        for result in results {
+            add_table_row(&mut destination, result);
+        }
+
+        Ok(destination)
+    }
+}
+
+fn start_table() -> Vec<u8> {
+    "| Benchmark | Mean [ms] | Min. [ms] | Max. [ms] |\n|----|----|----|----|\n"
+        .bytes()
+        .collect()
+}
+fn add_table_row(dest: &mut Vec<u8>, entry: &ExportEntry) {
+    dest.extend(
+        format!(
+            "| {} | {:.1} ± {:.1} | {:.1} | {:.1} |\n",
+            entry.command.replace("|", "\\|"),
+            entry.mean * MULTIPLIER,
+            entry.stddev * MULTIPLIER,
+            entry.min * MULTIPLIER,
+            entry.max * MULTIPLIER
+        ).as_bytes(),
+    );
+}
