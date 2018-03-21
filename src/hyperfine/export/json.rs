3351f52b1b0acd08364794914e8f77eaeb034606
sharkdp <davidpeter@web.de>
2018-03-21 22:56:01 +0100

    Add newline to JSON output, closes #49

--- src/hyperfine/export/json.rs
+++ src/hyperfine/export/json.rs
@@ -14,6 +14,11 @@ pub struct JsonExporter {}
 
 impl Exporter for JsonExporter {
     fn serialize(&self, results: &Vec<ExportEntry>) -> Result<Vec<u8>> {
-        to_vec_pretty(&HyperfineSummary { results }).map_err(|e| Error::new(ErrorKind::Other, e))
+        let mut output = to_vec_pretty(&HyperfineSummary { results });
+        for content in output.iter_mut() {
+            content.push(b'\n');
+        }
+
+        output.map_err(|e| Error::new(ErrorKind::Other, e))
     }
 }
