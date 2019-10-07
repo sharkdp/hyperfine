657fbc30f84c7cc86be145406614098e25dfb06b
Yasuaki Uechi <y@uechi.io>
2019-10-07 14:57:42 +0900

    scripts: add welch's t test

---
+++ scripts/welch_ttest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import argparse
+import json
+from scipy import stats
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="JSON file with benchmark results")
+args = parser.parse_args()
+
+with open(args.file) as f:
+    results = json.load(f)["results"]
+
+a, b = [x["command"] for x in results[:2]]
+X, Y = [x["times"] for x in results[:2]]
+
+print("Command 1: {}".format(a))
+print("Command 2: {}\n".format(b))
+
+t, p = stats.ttest_ind(X, Y, equal_var=False)
+th = 0.05
+dispose = p < th
+print("t = {}, p = {}".format(t, p))
+print("p < {} = {}\n".format(th, dispose))
+if dispose:
+    print("There is a difference between the two benchmarks.")
+else:
+    print("The two benchmarks are almost the same.")
