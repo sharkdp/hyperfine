02962d1caba8a361e59fde8b0969f05848c489f9
sharkdp <davidpeter@web.de>
2019-10-07 19:04:33 +0200

    Use doc-comment for advanced_statistics.py script

--- scripts/plot_benchmark_results.py
+++ scripts/plot_benchmark_results.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
-#
-# This script `hyperfine` benchmark results as a box and whisker plot.
-#
-# Quoting from the matplotlib documentation:
-#     The box extends from the lower to upper quartile values of the data, with
-#     a line at the median. The whiskers extend from the box to show the range
-#     of the data. Flier points are those past the end of the whiskers.
-#
+
+"""This program shows `hyperfine` benchmark results as a box and whisker plot.
+
+Quoting from the matplotlib documentation:
+    The box extends from the lower to upper quartile values of the data, with
+    a line at the median. The whiskers extend from the box to show the range
+    of the data. Flier points are those past the end of the whiskers.
+"""
 
 import argparse
 import json
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("file", help="JSON file with benchmark results")
 args = parser.parse_args()
 
