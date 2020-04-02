d441eefd1d2dcbb984e26eed842e6da53bb1e33e
sharkdp <davidpeter@web.de>
2020-04-02 18:59:21 +0200

    plot_histogram.py: add --type and --bins

--- scripts/plot_histogram.py
+++ scripts/plot_histogram.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("file", help="JSON file with benchmark results")
 parser.add_argument("--title", help="Plot title")
+parser.add_argument("--bins", help="Number of bins (default: auto)")
+parser.add_argument(
+    "--type", help="Type of histogram (*bar*, barstacked, step, stepfilled)"
+)
 args = parser.parse_args()
 
 with open(args.file) as f:
@@ -21,12 +25,11 @@ all_times = [b["times"] for b in results]
 t_min = np.min(list(map(np.min, all_times)))
 t_max = np.max(list(map(np.max, all_times)))
 
+bins = int(args.bins) if args.bins else "auto"
+histtype = args.type if args.type else "bar"
+
 plt.hist(
-    all_times,
-    label=commands,
-    bins="auto",
-    histtype="bar",
-    range=(t_min, t_max),
+    all_times, label=commands, bins=bins, histtype=histtype, range=(t_min, t_max),
 )
 plt.legend(prop={"family": ["Source Code Pro", "Fira Mono", "Courier New"]})
 
