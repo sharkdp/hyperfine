9ca3fa132c8ba2fe3694733ade407f43cb9763d7
Martin Junghanns <martin.junghanns@neo4j.com>
2020-10-17 15:23:45 +0200

    Export intermediate results after each benchmark command

--- src/hyperfine/export/mod.rs
+++ src/hyperfine/export/mod.rs
@@ -69,7 +69,7 @@ impl ExportManager {
     }
 
     /// Write the given results to all Exporters contained within this manager
-    pub fn write_results(&self, results: Vec<BenchmarkResult>, unit: Option<Unit>) -> Result<()> {
+    pub fn write_results(&self, results: &[BenchmarkResult], unit: Option<Unit>) -> Result<()> {
         for e in &self.exporters {
             let file_content = e.exporter.serialize(&results, unit)?;
             write_to_file(&e.filename, &file_content)?;
