40d58c02bcc49b22f618df795f6930e64aacebe7
David Peter <mail@david-peter.de>
2021-08-24 21:58:05 +0200

    Move progress bar to separate module

--- src/internal.rs
+++ src/internal.rs
@@ -2,40 +2,12 @@ use std::cmp::Ordering;
 use std::iter::Iterator;
 
 use colored::*;
-use indicatif::{ProgressBar, ProgressStyle};
 
 use crate::benchmark_result::BenchmarkResult;
-use crate::options::OutputStyleOption;
 use crate::units::{Scalar, Second};
 
 /// Threshold for warning about fast execution time
 pub const MIN_EXECUTION_TIME: Second = 5e-3;
-
-#[cfg(not(windows))]
-const TICK_SETTINGS: (&str, u64) = ("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏ ", 80);
-
-#[cfg(windows)]
-const TICK_SETTINGS: (&str, u64) = (r"+-x| ", 200);
-
-/// Return a pre-configured progress bar
-pub fn get_progress_bar(length: u64, msg: &str, option: OutputStyleOption) -> ProgressBar {
-    let progressbar_style = match option {
-        OutputStyleOption::Basic | OutputStyleOption::Color => ProgressStyle::default_bar(),
-        _ => ProgressStyle::default_spinner()
-            .tick_chars(TICK_SETTINGS.0)
-            .template(" {spinner} {msg:<30} {wide_bar} ETA {eta_precise}"),
-    };
-
-    let progress_bar = match option {
-        OutputStyleOption::Basic | OutputStyleOption::Color => ProgressBar::hidden(),
-        _ => ProgressBar::new(length),
-    };
-    progress_bar.set_style(progressbar_style);
-    progress_bar.enable_steady_tick(TICK_SETTINGS.1);
-    progress_bar.set_message(msg.to_owned());
-
-    progress_bar
-}
 
 /// A max function for f64's without NaNs
 pub fn max(vals: &[f64]) -> f64 {
---
+++ src/progress_bar.rs
@@ -0,0 +1,29 @@
+use indicatif::{ProgressBar, ProgressStyle};
+
+use crate::options::OutputStyleOption;
+
+#[cfg(not(windows))]
+const TICK_SETTINGS: (&str, u64) = ("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏ ", 80);
+
+#[cfg(windows)]
+const TICK_SETTINGS: (&str, u64) = (r"+-x| ", 200);
+
+/// Return a pre-configured progress bar
+pub fn get_progress_bar(length: u64, msg: &str, option: OutputStyleOption) -> ProgressBar {
+    let progressbar_style = match option {
+        OutputStyleOption::Basic | OutputStyleOption::Color => ProgressStyle::default_bar(),
+        _ => ProgressStyle::default_spinner()
+            .tick_chars(TICK_SETTINGS.0)
+            .template(" {spinner} {msg:<30} {wide_bar} ETA {eta_precise}"),
+    };
+
+    let progress_bar = match option {
+        OutputStyleOption::Basic | OutputStyleOption::Color => ProgressBar::hidden(),
+        _ => ProgressBar::new(length),
+    };
+    progress_bar.set_style(progressbar_style);
+    progress_bar.enable_steady_tick(TICK_SETTINGS.1);
+    progress_bar.set_message(msg.to_owned());
+
+    progress_bar
+}
