f28a44c0cd81f2e253dddaeba4bde384417cb52f
David Peter <mail@david-peter.de>
2022-02-06 13:48:39 +0100

    Add more integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -31,3 +31,33 @@ fn fails_with_wrong_number_of_command_name_arguments() {
         .failure()
         .stderr(predicate::str::contains("Too many --command-name options"));
 }
+
+#[test]
+fn fails_for_unknown_command() {
+    hyperfine()
+        .arg("some-nonexisting-program-b5d9574198b7e4b12a71fa4747c0a577")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Command terminated with non-zero exit code",
+        ));
+}
+
+#[cfg(unix)]
+#[test]
+fn can_run_failing_commands_with_ignore_failure_option() {
+    hyperfine()
+        .arg("false")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "Command terminated with non-zero exit code",
+        ));
+
+    hyperfine()
+        .arg("--runs=1")
+        .arg("--ignore-failure")
+        .arg("false")
+        .assert()
+        .success();
+}
