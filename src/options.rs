sharkdp/hyperfine

commit 049d1138c94b1654a80b3afc164bcc5c672560f5

Don't output colours by default if TERM=dumb or NO_COLOR=1
This matches ZFS, and is mostly état d'art

Closes #542

src/options.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
-use std::{cmp, fmt, io};
+use std::{cmp, env, fmt, io};
 
 use anyhow::ensure;
 use atty::Stream;
@@ -298,6 +298,14 @@ impl Options {
                     || !atty::is(Stream::Stdout)
                 {
                     OutputStyleOption::Basic
+                } else if !(env::var_os("TERM")
+                    .map(|t| t != "unknown" && t != "dumb")
+                    .unwrap_or(false)
+                    && env::var_os("NO_COLOR")
+                        .map(|t| t.is_empty())
+                        .unwrap_or(true))
+                {
+                    OutputStyleOption::NoColor
                 } else {
                     OutputStyleOption::Full
                 }

commit f0f8b6827c9feff24e690e18e658ee02f3b9d9fc

Default to $TERM unset = colour on Win32
This otherwise respects all other configuration
(NO_COLOR, TERM=dumb, &c.), but it's very common for the environment to
be lacking TERM, which in that case usually means a pro-verbial
TERM=cmd.exe, which is colourful

Fixes: 049d1138c94b1654a80b3afc164bcc5c672560f5 ("Don't output colours
 by default if TERM=dumb or NO_COLOR=1")

src/options.rs
@@ -300,7 +300,7 @@ impl Options {
                     OutputStyleOption::Basic
                 } else if env::var_os("TERM")
                     .map(|t| t == "unknown" || t == "dumb")
-                    .unwrap_or(true)
+                    .unwrap_or(!cfg!(target_os = "windows"))
                     || env::var_os("NO_COLOR")
                         .map(|t| !t.is_empty())
                         .unwrap_or(false)