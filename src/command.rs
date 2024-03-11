6c774103d95e9be150a7924d92de215f97b09c2e
Hamir Mahal <hamirmahal@gmail.com>
2024-03-11 00:26:24 -0700

    refactor: replace repeated string allocation on each iteration with one `String` creation in `fold`, for efficiency

--- src/command.rs
+++ src/command.rs
@@ -59,10 +59,11 @@ impl<'a> Command<'a> {
     }
 
     pub fn get_name_with_unused_parameters(&self) -> String {
-        let parameters = self
-            .get_unused_parameters()
-            .map(|(parameter, value)| format!("{} = {}, ", parameter, value.to_string()))
-            .collect::<String>();
+        let parameters =
+            self.get_unused_parameters()
+                .fold(String::new(), |output, (parameter, value)| {
+                    output + &format!("{} = {}, ", parameter, value.to_string())
+                });
         let parameters = parameters.trim_end_matches(", ");
         let parameters = if parameters.is_empty() {
             "".into()
