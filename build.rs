6bff39b853c821f6f324c05f195ae192a60d961f
chikoyeat <manavrj.07@gmail.com>
2024-10-18 14:58:10 +0530

    fixed build file for latest version

--- build.rs
+++ build.rs
@@ -1,16 +1,13 @@
 use std::fs;
-
-use clap_complete::{generate_to, Shell};
+use std::path::PathBuf;
+use std::error::Error;
+use clap_complete::{generate_to, shells::Shell};
 
 include!("src/cli.rs");
 
-fn main() {
-    let var = std::env::var_os("SHELL_COMPLETIONS_DIR").or_else(|| std::env::var_os("OUT_DIR"));
-    let outdir = match var {
-        None => return,
-        Some(outdir) => outdir,
-    };
-    fs::create_dir_all(&outdir).unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let outdir = get_output_dir()?;
+    fs::create_dir_all(&outdir)?;
 
     let mut command = build_command();
     for shell in [
@@ -20,6 +17,17 @@ fn main() {
         Shell::PowerShell,
         Shell::Elvish,
     ] {
-        generate_to(shell, &mut command, "hyperfine", &outdir).unwrap();
+        generate_to(shell, &mut command, "hyperfine", &outdir)
+            .map_err(|e| format!("Failed to generate completions for {:?}: {}", shell, e))?;
     }
+
+    println!("Completions generated successfully in {:?}", outdir);
+    Ok(())
+}
+
+fn get_output_dir() -> Result<PathBuf, Box<dyn Error>> {
+    std::env::var_os("SHELL_COMPLETIONS_DIR")
+        .or_else(|| std::env::var_os("OUT_DIR"))
+        .map(PathBuf::from)
+        .ok_or_else(|| "Neither SHELL_COMPLETIONS_DIR nor OUT_DIR environment variable is set".into())
 }
