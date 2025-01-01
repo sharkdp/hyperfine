9645a2ba36bd0a19e19394c3528ce953f2c7744a
David Peter <mail@david-peter.de>
2025-01-01 22:57:29 +0100

    Unify TimingResult/TimerResult/Measurement

--- src/timer/mod.rs
+++ src/timer/mod.rs
@@ -16,24 +16,13 @@ use std::os::fd::AsFd;
 #[cfg(target_os = "windows")]
 use windows_sys::Win32::System::Threading::CREATE_SUSPENDED;
 
-use crate::benchmark::quantity::{Byte, Second};
+use crate::benchmark::measurement::Measurement;
 use wall_clock_timer::WallClockTimer;
 
 use std::io::Read;
-use std::process::{ChildStdout, Command, ExitStatus};
+use std::process::{ChildStdout, Command};
 
 use anyhow::Result;
-
-/// Used to indicate the result of running a command
-#[derive(Debug, Copy, Clone)]
-pub struct TimerResult {
-    pub time_wall_clock: Second,
-    pub time_user: Second,
-    pub time_system: Second,
-    pub memory_usage_byte: Byte,
-    /// The exit status of the process
-    pub status: ExitStatus,
-}
 
 /// Discard the output of a child process.
 fn discard(output: ChildStdout) {
@@ -67,7 +56,7 @@ fn discard(output: ChildStdout) {
 }
 
 /// Execute the given command and return a timing summary
-pub fn execute_and_measure(mut command: Command) -> Result<TimerResult> {
+pub fn execute_and_measure(mut command: Command) -> Result<Measurement> {
     #[cfg(not(windows))]
     let cpu_timer = self::unix_timer::CPUTimer::start();
 
@@ -93,14 +82,14 @@ pub fn execute_and_measure(mut command: Command) -> Result<TimerResult> {
         discard(output);
     }
 
-    let (status, time_user, time_system, memory_usage_byte) = cpu_timer.stop(child)?;
+    let (exit_status, time_user, time_system, memory_usage_byte) = cpu_timer.stop(child)?;
     let time_wall_clock = wallclock_timer.stop();
 
-    Ok(TimerResult {
+    Ok(Measurement {
         time_wall_clock,
         time_user,
         time_system,
-        memory_usage_byte,
-        status,
+        peak_memory_usage: memory_usage_byte,
+        exit_status,
     })
 }
